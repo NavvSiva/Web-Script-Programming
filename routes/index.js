2389f9bd51bd8cf632c35729627ffd61753ebe68
NavvSiva <abhinav.sivasakthi@ontariotechu.net>
2024-11-05 16:46:46 -0500

    added a layout.cjs and included a link to my github and to my linkedin

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', (req, res) => res.render('home', { title: 'Home' }));
-router.get('/about', (req, res) => res.render('about', { title: 'About Me' }));
-router.get('/projects', (req, res) => res.render('projects', { title: 'Projects' }));
-router.get('/contact', (req, res) => res.render('contact', { title: 'Contact Me' }));
+router.get('/', (req, res) => res.render('home', { title: 'Home', layout: 'layout' }));
+router.get('/about', (req, res) => res.render('about', { title: 'About Me', layout: 'layout' }));
+router.get('/projects', (req, res) => res.render('projects', { title: 'Projects', layout: 'layout' }));
+router.get('/contact', (req, res) => res.render('contact', { title: 'Contact Me', layout: 'layout' }));
 
 module.exports = router;
